bab8718150a7b53390024c3b64c2a8d2fa723cce
nathan.northcutt@gmail.com <nathan.northcutt@gmail.com@6606c15e-fd23-72b6-d48c-9d922336c1f8>
2010-09-15 21:38:34 +0000

    made some changes to everything it seems.  working with getting the status bars hooked up and basic tet rendering working.  still have a LONG ways to go.

--- trunk/MajorMudX/MajorMudX.UI/Views/StatusView.xaml.cs
+++ trunk/MajorMudX/MajorMudX.UI/Views/StatusView.xaml.cs
@@ -9,14 +9,58 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using System.ComponentModel;
 
 namespace MajorMudX.UI.Views
 {
-    public partial class StatusView : UserControl
+    public partial class StatusView : UserControl, INotifyPropertyChanged
     {
+        CharacterLayoutHostView _host;
+
         public StatusView()
         {
             InitializeComponent();
+            MaxHP = 34;
         }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            DependencyObject parent = Parent;
+            while (parent != null && !typeof(CharacterLayoutHostView).IsAssignableFrom(parent.GetType()))
+                parent = VisualTreeHelper.GetParent(parent);
+
+            if (parent != null)
+            {
+                _host = parent as CharacterLayoutHostView;
+                if (_host != null)
+                    _host.Status = this;
+            }
+        }
+
+        int _maxHp;
+        public int MaxHP
+        {
+            get { return _maxHp; }
+            set { _maxHp = value; Notify("MaxHP"); }
+        }
+
+        int _currentHp;
+        public int CurrentHP
+        {
+            get { return _currentHp; }
+            set { _currentHp = value; Notify("CurrentHP"); }
+        }
+
+        void Notify(string propName)
+        {
+            if (Dispatcher.CheckAccess())
+            {
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+            else Dispatcher.BeginInvoke(() => { Notify(propName); });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
--- trunk/MajorMudX/MajorMudX.UI/Views/TextWindowView.xaml.cs
+++ trunk/MajorMudX/MajorMudX.UI/Views/TextWindowView.xaml.cs
@@ -9,14 +9,107 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using MajorMudX.Core.Infrastructure;
+using MajorMudX.Core.UI.Text;
 
 namespace MajorMudX.UI.Views
 {
-    public partial class TextWindowView : UserControl
+    public partial class TextWindowView : UserControl, ITextRenderer
     {
+        List<IFormattedTextSegment> _incompleteSegments;
+        CharacterLayoutHostView _host;
+
         public TextWindowView()
         {
             InitializeComponent();
+            _incompleteSegments = new List<IFormattedTextSegment>();
+        }
+
+        private void LayoutRoot_GotFocus(object sender, RoutedEventArgs e)
+        {
+            this.tbxCaptureText.Focus();
+        }
+
+        private void tbxCaptureText_KeyDown(object sender, KeyEventArgs e)
+        {
+            string s = tbxCaptureText.Text;
+            if (e.Key == Key.Enter)
+                s += "\n";
+            else if (e.Key == Key.Back)
+                s += "\b";
+            ClearText();
+
+            if (_host != null)
+                _host.Write(s);
+        }
+
+        void ClearText()
+        {
+            if (Dispatcher.CheckAccess()) tbxCaptureText.Text = string.Empty;
+            else Dispatcher.BeginInvoke(() => { ClearText(); });
+        }
+
+        public void ProcessBackspace()
+        {
+            if (Dispatcher.CheckAccess())
+            {
+                if (tblkMainContent.Inlines.Count == 0) return;
+
+                Run current = tblkMainContent.Inlines[tblkMainContent.Inlines.Count - 1] as Run;
+                if (current == null) return;
+                if (current.Text.Length == 0) return;
+                current.Text = current.Text.Remove(current.Text.Length - 2);
+            }
+            else Dispatcher.BeginInvoke(() => { ProcessBackspace(); });
+        }
+
+        public void RenderText(IFormattedTextSegment segment)
+        {
+            if (Dispatcher.CheckAccess())
+            {
+                if (!segment.Complete)
+                {
+                    _incompleteSegments.Add(segment);
+                }
+                else
+                {
+                    int start = tblkMainContent.Inlines.Count - _incompleteSegments.Count;
+                    start = start < 0 ? 0 : start;
+
+                    for (int i = start; i < tblkMainContent.Inlines.Count; ++i)
+                    {
+                        Run prev = tblkMainContent.Inlines[i] as Run;
+                        if (prev == null) continue;
+                        prev.Foreground = new SolidColorBrush(segment.TextColor);
+                    }
+
+                    _incompleteSegments.Clear();
+                }
+
+                Run r = new Run();
+                r.Text = segment.Text;
+                r.Foreground = new SolidColorBrush(segment.TextColor);
+
+                tblkMainContent.Inlines.Add(r);
+
+                svMainContent.UpdateLayout();
+                svMainContent.ScrollToVerticalOffset(double.MaxValue);
+            }
+            else Dispatcher.BeginInvoke(() => { RenderText(segment); });
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            DependencyObject parent = Parent;
+            while (parent != null && !typeof(CharacterLayoutHostView).IsAssignableFrom(parent.GetType()))
+                parent = VisualTreeHelper.GetParent(parent);
+
+            if (parent != null)
+            {
+                _host = parent as CharacterLayoutHostView;
+                if (_host != null)
+                    _host.TextRenderer = this;
+            }
         }
     }
 }
