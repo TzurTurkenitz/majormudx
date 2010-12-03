b74d6c60634634d965b4726821e3c4b777127515
Nathan.Northcutt <nathan.northcutt@gmail.com>
2010-12-03 23:57:23 +0000

    added ad-hoc reflection view model builder.  this will be part of the overall strategy in the coming design to allow objects to be pre-built on app startup and then using dependency injection and custom object builders to allow runtime invocation and building.

---
+++ MMX/MMX.Core/API/Infrastructure/ViewModels/ViewModelAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace MMX.Core.API.Infrastructure.ViewModels
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ViewModelAttribute : Attribute
+    {
+        public string ID { get; set; }
+        public ViewModelCreationMethod CreationMethod { get; set; }
+    }
+
+    public enum ViewModelCreationMethod
+    {
+        Instance,
+        Singleton
+    }
+}
