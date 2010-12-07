cc7238c2dae534661791db958d70c7618ab32a6a
nathan.northcutt@gmail.com <nathan.northcutt@gmail.com@6606c15e-fd23-72b6-d48c-9d922336c1f8>
2010-12-07 16:44:07 +0000

    added initial bootstrapper class for taking care of initial loading

---
+++ trunk/MMX/MMX.Core/API/Infrastructure/Initialization/Bootstrapper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using MMX.Core.API.Infrastructure.ViewModels;
+using MMX.Core.API.Infrastructure.Control;
+
+namespace MMX.Core.API.Infrastructure.Initialization
+{
+    public class Bootstrapper
+    {
+        public static IController ShellController { get; set; }
+
+        public static void ReflectAssemblies(System.Windows.Deployment current)
+        {
+            List<Assembly> assemblies = current.Parts.Select(
+                    ap => Application.GetResourceStream(new Uri(ap.Source, UriKind.Relative))).Select(
+                        stream => new AssemblyPart().Load(stream.Stream)).ToList();
+
+            foreach (Assembly asm in assemblies)
+            {
+                foreach (Type t in asm.GetTypes().Where((at) => { return at.IsDefined(typeof(ViewModelAttribute), true); }))
+                {
+                    ViewModelAttribute vmAtt = t.GetCustomAttributes(true).FirstOrDefault(
+                        (a) => { return a is ViewModelAttribute; }) as ViewModelAttribute;
+
+                    object instance = Activator.CreateInstance(t);
+                    PropertyInfo pController = t.GetProperty("Controller", BindingFlags.Instance | BindingFlags.Public);
+                    if (pController != null)
+                        pController.SetValue(instance, ShellController, null);
+                    ViewModelLocator.Register(vmAtt.ID, instance);
+                }
+            }
+        }
+    }
+}
