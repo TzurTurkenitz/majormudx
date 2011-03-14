f27f2e975fd4e431c33e079c8963599c778e8e8a
Nathan.Northcutt <nathan.northcutt@gmail.com>
2011-03-14 17:51:58 +0000

    creating new working project with corrected api and other functionality.  need to rebuild all plumbing to allow for a real application.

---
+++ Working/MMX.Common/API/Communication/ITelnetSocket.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace MMX.Common.API.Communication
+{
+    public interface ITelnetSocket : IDisposable
+    {
+        void Open();
+        void Close();
+
+        int BufferSize { get; set; }
+
+        int Write(byte[] buffer, int offset, int length);
+
+        bool RegisterProcessor<T>(T processor)
+            where T : ITelnetProcessor;
+
+        event EventHandler<TelnetEventArgs> OnOpening;
+        event EventHandler<TelnetEventArgs> OnOpened;
+        event EventHandler<TelnetEventArgs> OnClosing;
+        event EventHandler<TelnetEventArgs> OnClosed;
+    }
+}
---
+++ Working/MMX.Common/API/Communication/TelnetEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace MMX.Common.API.Communication
+{
+    public class TelnetEventArgs : EventArgs
+    {
+        public bool Cancel { get; set; }
+    }
+}
