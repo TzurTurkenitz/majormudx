8d9fe3c8ee4d1d4b12b2d1f29db2a029b50e3ea9
Nathan.Northcutt@gmail.com <Nathan.Northcutt@gmail.com@6606c15e-fd23-72b6-d48c-9d922336c1f8>
2011-03-14 23:39:17 +0000

    added base class.  need to do more with it.

---
+++ trunk/Working/MMX.Common/API/UI/ViewModelBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using MMX.Common.API.Services;
+
+namespace MMX.Common.API.UI
+{
+    public abstract class ViewModelBase : IMMXViewModel
+    {
+        protected IServiceLocator _locator;
+
+        IServiceLocator IMMXViewModel.ServiceLocator
+        {
+            get { return _locator; }
+        }
+    }
+}
