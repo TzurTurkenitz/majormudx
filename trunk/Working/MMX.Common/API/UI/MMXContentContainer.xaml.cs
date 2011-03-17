6ea5b11b60e42eb0972b70f82e4bbd9345a31c82
Nathan.Northcutt@gmail.com <Nathan.Northcutt@gmail.com@6606c15e-fd23-72b6-d48c-9d922336c1f8>
2011-03-17 18:44:15 +0000

    adding containers to support view injection

---
+++ trunk/Working/MMX.Common/API/UI/MMXContentContainer.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace MMX.Common.API.UI
+{
+    public sealed partial class MMXContentContainer : MMXContentSite
+    {
+        public MMXContentContainer()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            LayoutRoot.Background = this.Background;
+            
+            LayoutRoot.UpdateLayout();
+        }
+    }
+}
