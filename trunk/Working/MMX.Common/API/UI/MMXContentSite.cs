5da6ef4a54da7c8f545a70c0c2fc88d4e55a2e1c
Nathan.Northcutt@gmail.com <Nathan.Northcutt@gmail.com@6606c15e-fd23-72b6-d48c-9d922336c1f8>
2011-03-17 21:05:36 +0000

    added container management, going to test in a little bit

--- trunk/Working/MMX.Common/API/UI/MMXContentSite.cs
+++ trunk/Working/MMX.Common/API/UI/MMXContentSite.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
@@ -11,15 +12,37 @@ using System.Windows.Shapes;
 
 namespace MMX.Common.API.UI
 {
-    public class MMXContentSite : UserControl
+    public class MMXContentSite : UserControl, IMMXContentContainer
     {
+        Dictionary<string, UserControl> _views;
+        protected Canvas _root;
+
         public MMXContentSite()
         {
-            this.Loaded += new RoutedEventHandler(OnLoaded);
+            _views = new Dictionary<string, UserControl>();
+            _root = null;
         }
 
-        protected virtual void OnLoaded(object sender, RoutedEventArgs e)
+        bool IMMXContentContainer.Register<T>(T obj, string id)
         {
+            if (!_views.ContainsKey(id))
+                _views.Add(id, null);
+
+            _views[id] = obj;
+
+            return true;
+        }
+
+        bool IMMXContentContainer.Remove(string id)
+        {
+            return _views.Remove(id);
+        }
+
+        void IMMXContentContainer.Activate(string id)
+        {
+            if (_root != null && _views.ContainsKey(id))
+                _root.Dispatcher.BeginInvoke(
+                    () => { _root.Children.Clear(); _root.Children.Add(_views[id]); });
         }
     }
 }
