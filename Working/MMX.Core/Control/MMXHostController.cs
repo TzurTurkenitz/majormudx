fdae526a9568129d1215ee53fe75e4744d4fc39d
Nathan.Northcutt <nathan.northcutt@gmail.com>
2011-03-17 23:45:29 +0000

    need to figure out why binding isn't working

--- Working/MMX.Core/Control/MMXHostController.cs
+++ Working/MMX.Core/Control/MMXHostController.cs
@@ -11,6 +11,8 @@ using System.Windows.Shapes;
 using MMX.Common.API.Control;
 using MMX.Common.API.Infrastructure;
 using MMX.Common.API.Services;
+using MMX.Core.UI;
+using MMX.Common.API.UI;
 
 namespace MMX.Core.Control
 {
@@ -23,6 +25,16 @@ namespace MMX.Core.Control
         public MMXHostController([MMXInjectNew(InjectionType = typeof(IServiceLocator))] IServiceLocator locator)
         {
             _locator = locator;
+
+            new ViewModelLocator()[ServiceConstants.MMXHostVM] = new MMXHostViewModel();
+
+            (new ViewModelLocator()[ServiceConstants.MMXHostVM] as MMXHostViewModel).Container.Register(new TestView(), "test");
+            (new ViewModelLocator()[ServiceConstants.MMXHostVM] as MMXHostViewModel).Container.Activate("test");
+
+            HostUI = new MMXHost();
+            
         }
+
+        public MMXHost HostUI { get; set; }
     }
 }
