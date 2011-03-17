8964516f25992cb4367327adea452fea4a626292
Nathan.Northcutt <nathan.northcutt@gmail.com>
2011-03-17 23:24:07 +0000

    added controller injection and some basic constructor parameter injection methods.  validated that i can create a mmxhost controller.  need to work on how to tell the controller what objects to create (views, viewmodels, etc)

---
+++ Working/MMX.Common/API/Infrastructure/MMXDefaultConstructorAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace MMX.Common.API.Infrastructure
+{
+    [AttributeUsage(AttributeTargets.Constructor)]
+    public class MMXDefaultConstructorAttribute : Attribute
+    {
+
+    }
+}
---
+++ Working/MMX.Core/Control/MMXHostController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using MMX.Common.API.Control;
+using MMX.Common.API.Infrastructure;
+using MMX.Common.API.Services;
+
+namespace MMX.Core.Control
+{
+    [MMXController]
+    public class MMXHostController : IMMXController
+    {
+        IServiceLocator _locator;
+
+        [MMXDefaultConstructor]
+        public MMXHostController([MMXInjectNew(InjectionType = typeof(IServiceLocator))] IServiceLocator locator)
+        {
+            _locator = locator;
+        }
+    }
+}
